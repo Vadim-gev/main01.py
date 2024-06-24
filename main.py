c9d7dd209a938214411fabb40bfb7682d10ac1c4
Vadim-gev <vadim_gevorkian@mail.ru>
2024-06-24 17:19:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+print('Длина слова')
+a = 'print'
+print(len(a))
+print('Суммы и разности')
+first = 5
+second = 2
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+print('Среднее арифметическое')
+first = 1
+second = 2
+third = 3
+mean = ((1 + 2 + 3) // 3)
+print(mean)
+print('Простые строчки')
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+print('Сложная формула')
+a = 2
+b = 4
+c = 6
+f = (a * b) + (a * c)
+h = f ** 3 // 2
+print(h)
